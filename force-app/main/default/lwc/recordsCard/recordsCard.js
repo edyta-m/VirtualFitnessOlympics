8b871cad8c12ce7c92a4cc90fa9721cc1d773f2e
Edith Molda <edith.22.mary@gmail.com>
2021-04-25 12:23:01 -0500

    Additions to recordsCard

--- force-app/main/default/lwc/recordsCard/recordsCard.js
+++ force-app/main/default/lwc/recordsCard/recordsCard.js
@@ -6,10 +6,10 @@ import STEPS from '@salesforce/schema/Contact.Steps__c';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
 
 const COLUMNS = [
-    { label: 'Team', fieldName: TEAM.fieldApiName, type: 'text' },
-    { label: 'First Name', fieldName: FIRST_NAME.fieldApiName, type: 'text' },
-    { label: 'Last Name', fieldName: LAST_NAME.fieldApiName, type: 'text' },
-    { label: 'Steps', fieldName: STEPS.fieldApiName, type: 'number' }
+    { label: 'Team', fieldName: TEAM.fieldApiName, type: 'text', editable: true },
+    { label: 'First Name', fieldName: FIRST_NAME.fieldApiName, type: 'text', editable: true },
+    { label: 'Last Name', fieldName: LAST_NAME.fieldApiName, type: 'text', editable: true },
+    { label: 'Steps', fieldName: STEPS.fieldApiName, type: 'number', editable: true }
 ];
 
 export default class RecordsCard extends LightningElement {
